605b994f7923c8508886c7fc83a8fb200bca3dd4
Stephen-Keith-Lubega <stephenkeith.lubega@my.jcu.edu.au>
2025-09-27 14:03:13 +1000

    Add score program

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -2,13 +2,23 @@
 CP1404/CP5632 - Practical
 Program to determine score status
 """
+def main():
+    """Get numeric score and display its status"""
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+    score = float(input("Enter score: "))
+    (determine_status(score))
+
+
+def determine_status(score):
+    """determine_status"""
+    if score < 0 or score > 100:
+        print("Invalid score")
+    elif score >= 90:
+        print("Excellent")
+    elif score >= 50:
+        print("Passable")
+    else:
+        print("Bad")
+
+
+main()
