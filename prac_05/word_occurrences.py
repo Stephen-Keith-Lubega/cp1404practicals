c56bca32d5abad77d8879df6cbb63027861fc05d
Stephen-Keith-Lubega <stephenkeith.lubega@my.jcu.edu.au>
2025-10-18 19:54:53 +1000

    count word occurrences

---
+++ prac_05/word_occurrences.py
@@ -0,0 +1,18 @@
+"""
+Word Occurrences
+Estimate: 25 minutes
+Actual:    minutes
+"""
+
+text = input("Text: ")
+word_counts = {}
+
+words = text.split()
+for word in words:
+    if word in word_counts:
+        word_counts[word] += 1
+    else:
+        word_counts[word] = 1
+
+for word, count in word_counts.items():
+    print(f"{word} : {count}")
