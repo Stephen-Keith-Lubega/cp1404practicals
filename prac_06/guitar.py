78bfc50092408ce0d15b752eeeb4003f85439a7e
Stephen-Keith-Lubega <stephenkeith.lubega@my.jcu.edu.au>
2025-11-02 11:15:09 +1000

    Complete Guitar class and testing

---
+++ prac_06/guitar.py
@@ -0,0 +1,32 @@
+"""
+Guitar class for storing guitar details.
+Estimated time: 30 minutes
+Actual time:
+"""
+
+class Guitar:
+    """Represent a Guitar object"""
+    def __init__(self, name="",year=0,cost=0):
+        """Initialize guitar instance
+
+        name: sting, guitar name
+        year: int, year guitar was made
+        cost: float, guitar cost
+        """
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        """Return string representation of Guitar."""
+        return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
+
+    def get_age(self):
+        """Return age of guitar"""
+        current_year = 2022
+        return current_year - self.year
+
+
+    def determine_weather_vintage(self):
+        """Determine weather vintage or not """
+        return self.get_age() >= 50
